727ba951e253d348ef7fb0d0e1ddd5809a555f9a
vnlopes <viniciuslopesdasilva01@gmail.com>
2024-03-19 00:24:49 -0300

    add js and style to project

---
+++ function/script.js
@@ -0,0 +1,38 @@
+let time = 15;
+let count = 6;
+
+const init = () =>{
+    document.querySelector('.main-quiz').style = 'right: 0; visibility: visible;'
+    document.querySelector('.welcome').style = 'opacity: 0;'
+    let regress = document.querySelector('.regressive')
+    regress.style = 'color: white; opacity: 100%; z-index: 10; font-weight: 900; font-size: 6rem;'
+    
+    setInterval(() => {
+        if(count == 0){
+            count = 0;
+            regress.style = 'opacity: 0;'
+        } else{
+            count --
+            regress.innerHTML = count
+            console.log(contain)
+            console.log(regress)
+        }
+
+        
+    }, 1000);
+
+
+    setTimeout(() => {
+        fift = setInterval(() => {
+            if(time === 0){
+                clearInterval(fift)
+                time = 0;
+                alert('Seu tempo acabou!')
+            } else if(
+                time --
+            )
+            document.querySelector('.time span').innerHTML = time;
+        }, 1000);
+    }, 5000);
+
+}
